7a3a946d0bc52a81a703ace397c31b00f2ba2754
Mubeen31 <qs6272527@gmail.com>
2023-02-06 20:02:26 +0000

    first commit

--- data.py
+++ data.py
@@ -1,10 +1,1 @@
-config = {
-  "apiKey": "AIzaSyCPzA7m7sAGLkVdfv3Sn5JpvQPaenbzukk",
-  "authDomain": "home-gas-leakage.firebaseapp.com",
-  "databaseURL": "https://home-gas-leakage-default-rtdb.firebaseio.com",
-  "projectId": "home-gas-leakage",
-  "storageBucket": "home-gas-leakage.appspot.com",
-  "messagingSenderId": "1082065895502",
-  "appId": "1:1082065895502:web:a923481b50ba4cabc2031f",
-  "measurementId": "G-V201KDVM92"
-}
+config = {}
